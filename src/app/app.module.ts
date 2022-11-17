e8ef280f02edbcaa62b631fb5c84f95200792b82
Reginaraswari21 <rnaraswari@gmail.com>
2022-11-17 21:54:07 +0700

    41, 89, 92

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http';
+import { Network } from '@ionic-native/network/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+ declarations: [AppComponent],
+ imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+ providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+ bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { 
+    
+}
